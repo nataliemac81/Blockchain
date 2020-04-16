c5863ddfd65d6851e913c5ee1e8eed826c242cc9
Natalie McCroy <nataliemccroy@gmail.com>
2020-04-16 15:02:32 -0500

    blockchain gp completed

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -31,13 +31,19 @@ class Blockchain(object):
         """
 
         block = {
-            # TODO
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.last_block),
         }
 
         # Reset the current list of transactions
+        self.current_transactions = []
         # Append the chain to the block
+        self.chain.append(block)
         # Return the new block
-        pass
+        return block
 
     def hash(self, block):
         """
@@ -56,17 +62,21 @@ class Blockchain(object):
         # or we'll have inconsistent hashes
 
         # TODO: Create the block_string
+        string_object = json.dumps(block, sort_keys=True)
+        # encode block string to byte string
+        block_string = string_object.encode()
 
         # TODO: Hash this string using sha256
+        raw_hash = hashlib.sha256(block_string)
 
         # By itself, the sha256 function returns the hash in a raw string
         # that will likely include escaped characters.
         # This can be hard to read, but .hexdigest() converts the
         # hash to a string of hexadecimal characters, which is
         # easier to work with and understand
-
+        hash_string = raw_hash.hexdigest()
         # TODO: Return the hashed block string in hexadecimal format
-        pass
+        return hash_string
 
     @property
     def last_block(self):
@@ -81,8 +91,12 @@ class Blockchain(object):
         :return: A valid proof for the provided block
         """
         # TODO
-        pass
+        block_string = json.dumps(block, sort_keys=True)
+        proof = 0
+        while self.valid_proof(block_string, proof) is False:
+            proof += 1
         # return proof
+        return proof
 
     @staticmethod
     def valid_proof(block_string, proof):
@@ -97,9 +111,16 @@ class Blockchain(object):
         :return: True if the resulting hash is a valid proof, False otherwise
         """
         # TODO
-        pass
-        # return True or False
+        print(f'Checking if {proof} is valid...')
+        guess = block_string + str(proof)
+        guess = guess.encode()
+        
+        hash_value = hashlib.sha256(guess).hexdigest()
+        print(hash_value)
 
+        
+        # return True or False
+        return hash_value[:3] == '000'
 
 # Instantiate our Node
 app = Flask(__name__)
@@ -111,14 +132,29 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 
+@app.route('/', methods=['GET'])
+def hello_world():
+    response = {
+        'text': 'hello world'
+    }
+    return jsonify(response), 200
+
+
 @app.route('/mine', methods=['GET'])
 def mine():
     # Run the proof of work algorithm to get the next proof
+    print("Let's mine a block!")
+    proof = blockchain.proof_of_work(blockchain.last_block)
+    print(f'After a long process, we got a value {proof}')
 
+    
+    
     # Forge the new Block by adding it to the chain with the proof
-
+    
+    new_block = blockchain.new_block(proof)
     response = {
         # TODO: Send a JSON response with the new block
+        'block': new_block
     }
 
     return jsonify(response), 200
@@ -128,6 +164,8 @@ def mine():
 def full_chain():
     response = {
         # TODO: Return the chain and its current length
+        'len': len(blockchain.chain),
+        'chain': blockchain.chain
     }
     return jsonify(response), 200
 
